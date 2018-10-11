cc5da27df86977a00b70959f846a37789bb56e60
William Pei Yuan <williampeiyuan@gmail.com>
2018-10-10 21:06:28 -0400

    Navigating with React Router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/(:filter)" component={App} />
+    </Router>
   </Provider>
 );
 
+Root.PropTypes = {
+  store: PropTypes.object.isRequired
+};
+
 export default Root;
+
+// <Route path="/(:filter)" component={App} /> (:filter) -> means that its optional '/' will return 'SHOW ALL'
